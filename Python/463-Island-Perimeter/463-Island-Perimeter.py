be762514784864bd782c83a477acefde73ccebfe
Pedro Carvalho <125613555+PedroCarvalho8@users.noreply.github.com>
2024-04-18 00:39:07 -0300

    Create 463-Island-Perimeter.py

---
+++ Python/463-Island-Perimeter/463-Island-Perimeter.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                parameter = []
+                if grid[i][j] == 1: 
+                    perimeter += 4
+                
+                    if i!=len(grid)-1: parameter.append(grid[i+1][j]) 
+                    if i!= 0: parameter.append(grid[i-1][j])
+                    if j!=len(grid[0])-1: parameter.append(grid[i][j+1])
+                    if j!=0: parameter.append(grid[i][j-1])
+
+                for k in parameter:
+                    if (k == 1):
+                        perimeter -= 1
+        
+        return perimeter
+        
